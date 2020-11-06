3f2f48e157595120e75605a5b5280b642dfceb8e
myllyma <matti.myllymaki83@gmail.com>
2020-11-06 11:35:35 +0200

    Refactoring and implementation of correct-answer- mark

--- src/App.js
+++ src/App.js
@@ -2,94 +2,73 @@ import './App.css';
 import {useState, useEffect} from 'react';
 import axios from 'axios';
 import UIDGenerator from 'uid-generator';
+import Paper from '@material-ui/core/Paper';
+import Button from '@material-ui/core/Button';
+import Checkbox from '@material-ui/core/Checkbox';
 
 const Header = () => {
   return (
-    <ul className="Header">
-      <li>Tentit</li>
-      <li>Tietoa sovelluksesta</li>
-      <li>Poistu</li>
-    </ul>
+    <header className="Header">
+      <nav className="navBar">
+        <div className="navBarItem">Tentit</div>
+        <div className="navBarItem">Tietoa sovelluksesta</div>
+        <div className="navBarItem exitButton">Poistu</div>
+      </nav>
+    </header>
   );
 }
 
 const CourseSelectionList = ({courseData, programState, handleCourseSelection}) => {
   return (
-    programState.courses ?
-    <ul className="CourseSelectionList">
+    <div className="CourseSelectionList">
       {courseData.map((course, courseIndex) => 
-      <li key={programState.courses[courseIndex].id} onClick={handleCourseSelection(courseIndex)}>{course.courseName}</li>
+      <Button color="primary" className="CourseSelectionListItem" key={programState.courses[courseIndex].id} onClick={handleCourseSelection(courseIndex)}>{course.courseName}</Button>
       )}
-    </ul>
-    :
-    <div></div>
+    </div>
   );
 }
 
-const Question = ({question, programState, handleAnswerSelection}) => {
+const Question = ({question, programState, showAnswers, handleAnswerSelection}) => {
+  let showDogMark = true;
+  programState.answers.forEach((userAnswer, index) => { if (userAnswer.checked !== question.answers[index].correctAnswer) {showDogMark = false;}});
+
   return(
-    <div className="Question">
-      <p>{question.questionString}</p>
+    <Paper className="Question">
+      <p>{question.questionString} {showAnswers && showDogMark && <img className="dogImage" src="dog_image.png" alt="dog head"/>}</p>
       {question.answers.map((answer, answerIndex) => 
         <div key={programState.answers[answerIndex].id}>
-          <input type="checkbox" onChange={handleAnswerSelection(answerIndex)} checked={programState.answers[answerIndex].checked}/>{answer.answerString}
+          <Checkbox color="primary" onChange={handleAnswerSelection(answerIndex)} checked={programState.answers[answerIndex].checked} readOnly={true}/>
+          {showAnswers && <Checkbox color="secondary" checked={question.answers[answerIndex].correctAnswer} readOnly={true}/>}
+          {answer.answerString}
         </div>
       )}
-    </div>
+    </Paper>
   );
 }
 
-const ExamQuestions = ({questions, programState, handleAnswerSelection}) => {
-  return (
-    <div className="ExamQuestions">
-      {questions.questions.map((question, questionIndex) => 
-        <Question
-          key={programState.questions[questionIndex].id}
-          programState={programState.questions[questionIndex]}
-          question={question}
-          handleAnswerSelection={handleAnswerSelection(questionIndex)}/>
-      )}
-    </div>
-  );
-}
-
-const ExamQuestionsAnswers = () => {
-  return(
-    <div className="ExamQuestionsAnswers">
-
-    </div>
-  );
-}
-
+// Displays the data for one course's exam
 const CourseExamQuestions = ({courseData, programState, handleAnswerSelection, handleFinishAnswering}) => {
-  if (!programState.showAnswers) {
-    return(
-      programState.courses ?
-      <div className="CourseExamQuestions">
-        <ExamQuestions
-          questions={courseData[programState.activeCourse]}
-          programState={programState.courses[programState.activeCourse]}
-          handleAnswerSelection={handleAnswerSelection(programState.activeCourse)}/>
-        <button onClick={handleFinishAnswering}>Näytä vastaukset</button>
+  return(
+    <div className="CourseExamQuestions">
+      <div className="ExamQuestions">
+        {courseData[programState.activeCourse].questions.map((question, questionIndex) => {
+          return(
+          <Question
+            key={programState.courses[programState.activeCourse].questions[questionIndex].id}
+            question={question}
+            programState={programState.courses[programState.activeCourse].questions[questionIndex]}
+            showAnswers={programState.showAnswers}
+            handleAnswerSelection={handleAnswerSelection(programState.activeCourse, questionIndex)}/>
+          )
+        }
+        )}
       </div>
-      :
-      <div></div>
-    );
-  } else {
-    return(
-      programState.courses ?
-      <div className="CourseExamQuestions">
-        <ExamQuestionsAnswers
-          questions={courseData[programState.activeCourse]}
-          programState={programState.courses[programState.activeCourse]}
-          handleAnswerSelection={handleAnswerSelection(programState.activeCourse)}/>
-      </div>
-      :
-      <div></div>
-    );
-  }
+      {!programState.showAnswers && <Button variant="contained" color="primary" onClick={handleFinishAnswering}>Näytä vastaukset</Button>}
+    </div>
+  );
 }
 
+// Creates a program state object from given course data. Program state keeps track of user's selections in course questionnaires.
 const formNewProgramState = (courseData, UIDGenerator) => {
   return courseData.map((course) => ({                 // Map a course's questions to UI drawing list
       questions: course.questions.map((question) => ({ // Map questions of a course to UI drawing list
@@ -108,18 +87,29 @@ const App = () => {
   const [programState, setProgramState] = useState({});
 
   useEffect(() => {
-    axios
-      .get('http://localhost:3001/kurssit')
-      .then((response) => {
-        const newProgramState = {};
-        newProgramState.activeCourse = 0;
-        newProgramState.UIDGenerator = new UIDGenerator();
-        newProgramState.showAnswers = false;
-        newProgramState.courses = formNewProgramState(response.data, newProgramState.UIDGenerator);
-        setCourseData(response.data);
-        setProgramState(newProgramState);
-      }
-    );
+    /*
+    const localStorageState = window.localStorage.getItem("programState");
+    const localStorageData = window.localStorage.getItem("courseData");
+    if (localStorageState && localStorageData) {
+      setCourseData(JSON.parse(localStorageState));
+      setProgramState(JSON.parse(localStorageData));
+    } else {
+    */
+      axios
+        .get('http://localhost:3001/kurssit')
+        .then((response) => {
+          const newProgramState = {};
+          newProgramState.activeCourse = 0;
+          newProgramState.UIDGenerator = new UIDGenerator();
+          newProgramState.showAnswers = false;
+          newProgramState.courses = formNewProgramState(response.data, newProgramState.UIDGenerator);
+          setCourseData(response.data);
+          setProgramState(newProgramState);
+          window.localStorage.setItem("courseData", JSON.stringify(response.data));
+          window.localStorage.setItem("programState", JSON.stringify(newProgramState));
+        }
+      );
+    //}
   }, []);
 
   const handleCourseSelection = (courseIndex) => () => {
@@ -127,32 +117,39 @@ const App = () => {
     newProgramState.activeCourse = courseIndex;
     newProgramState.showAnswers = false;
     setProgramState(newProgramState);
+    window.localStorage.setItem("programState", JSON.stringify(newProgramState));
   }
 
-  const handleAnswerSelection = (courseIndex) => (questionInedex) => (answerIndex) => () => {
+  const handleAnswerSelection = (courseIndex, questionInedex) => (answerIndex) => () => {
     const newProgramState = JSON.parse(JSON.stringify(programState));
     newProgramState.courses[courseIndex].questions[questionInedex].answers[answerIndex].checked = !newProgramState.courses[courseIndex].questions[questionInedex].answers[answerIndex].checked;
     setProgramState(newProgramState);
+    window.localStorage.setItem("programState", JSON.stringify(newProgramState));
   }
 
   const handleFinishAnswering = () => {
     const newProgramState = JSON.parse(JSON.stringify(programState));
     newProgramState.showAnswers = true;
     setProgramState(newProgramState);
+    window.localStorage.setItem("programState", JSON.stringify(newProgramState));
   }
 
   return (
     <div className="App">
       <Header/>
-      <CourseSelectionList 
-        courseData={courseData}
-        programState={programState}
-        handleCourseSelection={handleCourseSelection}/>
-      <CourseExamQuestions
-        courseData={courseData}
-        programState={programState}
-        handleAnswerSelection={handleAnswerSelection}
-        handleFinishAnswering={handleFinishAnswering}/>
+      <main className="mainContent">
+        {courseData.length > 0 && programState.courses && 
+          <CourseSelectionList 
+            courseData={courseData}
+            programState={programState}
+            handleCourseSelection={handleCourseSelection}/>}
+        {courseData.length > 0 && programState.courses && 
+          <CourseExamQuestions
+            courseData={courseData}
+            programState={programState}
+            handleAnswerSelection={handleAnswerSelection}
+            handleFinishAnswering={handleFinishAnswering}/>}
+      </main>
     </div>
   );
 }
