edac16013b88baeaf5416e686385fec346e99a2b
myllyma <matti.myllymaki83@gmail.com>
2021-06-01 18:34:06 +0300

    Fix: Removed an unnecessary component.

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,7 +1,6 @@
 require("dotenv").config();
 const server = require("./server")
-const httpServer = require("http").createServer(server);
 
-httpServer.listen(process.env.PORT, () => {
+server.listen(process.env.PORT, () => {
   console.log(`Server is running on port ${process.env.PORT}`);
 });
