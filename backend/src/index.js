5b971ede04d8ede0673c501183110f0963f704c2
myllyma <matti.myllymaki83@gmail.com>
2021-06-01 18:34:39 +0300

    FEA: Added information to server startup message.

--- backend/src/index.js
+++ backend/src/index.js
@@ -2,5 +2,5 @@ require("dotenv").config();
 const server = require("./server")
 
 server.listen(process.env.PORT, () => {
-  console.log(`Server is running on port ${process.env.PORT}`);
+  console.log(`Server is running on port ${process.env.PORT} in ${process.env.NODE_ENV} mode`);
 });
